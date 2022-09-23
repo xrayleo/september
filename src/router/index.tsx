88602903c4e9b4e5c547677b6779cb3ff3418f51
anyways <youqingxw@163.com>
2022-09-23 16:32:02 +0800

    fixed issue with 404 being reported after nginx refresh

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '@views/Home'
 import About from '@views/About'
+import Help from '@views/Help'
+import Winners from '@views/Winners'
 
 function AppRouter() {
     return (
@@ -8,6 +10,8 @@ function AppRouter() {
             <Routes>
                 <Route path="*" element={ <Home /> }></Route>
                 <Route path="/about" element={ <About /> }></Route>
+                <Route path="/help" element={ <Help /> }></Route>
+                <Route path="/winners" element={ <Winners /> }></Route>
             </Routes>
         </BrowserRouter>
     );
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
-import reactLogo from '../../assets/react.svg';
-import './index.scss';
+import reactLogo from '../../assets/react.svg'
+import './index.scss'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
+  
+  const nav = useNavigate();
+  const go = (url : string) => {
+    nav(url);
+  }
   
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <h1 className="text-4xl mb-5">Home Page</h1>
+      <div>
+        <Button variant="contained" onClick={()=>go('about')}>跳转到About页面</Button><br/><br/>
+        <Button variant="contained" onClick={()=>go('winners')}>跳转到Winners页面</Button><br/><br/>
+        <Button variant="contained" onClick={()=>go('help')}>跳转到Help页面</Button>
       </div>
     </div>
   )
